9d019259331f678f12533e210cf19d4abd24ec42
Gustav-Br <99103909+Gustav-Br@users.noreply.github.com>
2023-11-22 17:08:34 -0300

    modificando rutas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
-
+const getProductoById = require('./controllers/productController');
+const deleteById = require('./controllers/deleteController');
+const postProducto = require('./controllers/postProductController');
 
 const router = Router();
 
@@ -8,7 +10,6 @@ router.get('/producto/:id', getProductoById);
 router.post('/register', postUser);
 router.post('/login', loginControler);
 router.delete('/delete', deleteById);
-
-
+router.post('/productos', postProducto);
 
 module.exports = router;
